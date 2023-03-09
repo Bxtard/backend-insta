0d006751367ca9999c3db29933ab7e0c22d9aaec
Bryan <ee.mxtrd@gmail.com>
2023-03-09 13:21:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const connectDb = require('./database');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.listen(PORT, async () => {
+  // Configure express
+  app.use(cors());
+  app.use(express.json());
+  app.use(morgan('dev'));
+
+  // Connect to database
+  await connectDb();
+
+  // Configure routes
+  app.get('/', (req, res) => {
+    return res.status(200).json({ message: 'OK' });
+  });
+
+  console.log(
+    `Server running on port http://localhost:${PORT} in ${NODE_ENV} mode`
+  );
+});
